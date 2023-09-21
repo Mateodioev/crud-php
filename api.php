6f18043b1120dd73cd2a300993936443412e0e70
Mateo O. <orehurcomito@gmail.com>
2023-09-21 11:23:14 -0500

    Add code to login

--- api.php
+++ api.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__.'/src/db.php';
+
 if ($_SERVER["REQUEST_METHOD"] !== 'POST') {
     redirect("index.php");
 }
@@ -13,11 +15,35 @@ $action = $_POST['action'] ?? 'invalid';
 
 switch ($action) {
     case 'login':
-        echo 'Buscando';
-        break;
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+        $result = Db::getInstance()->query('SELECT * FROM usuarios WHERE correo = ? OR username = ?', [$user, $user])->fetch(PDO::FETCH_ASSOC);
+
+        $password = $result['password'] ?? null;
+        $exists = $password !== null && matchPasswords($pass, $password);
+
+        if ($exists) {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $result['username'];
+            redirect('index.php');
+        } else {
+            die('Usuario o contraseña incorrectos');
+        }
     case 'signup':
-        echo 'Registrando';
-        break;
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+        $correo = $_POST['email'];
+
+        try {
+            Db::getInstance()->exec('INSERT INTO usuarios (username, `password`, correo) VALUES (?, ?, ?)', [
+                $user, encodePassword($pass), $correo
+            ]);
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $user;
+            redirect('index.php');
+        } catch (\Throwable $th) {
+            die('Error al crear el usuario');
+        }
     default:
         redirect('404.php');
         break;
@@ -30,4 +56,8 @@ function redirect(string $url): never {
 
 function encodePassword(string $password): string {
     return password_hash($password, PASSWORD_DEFAULT);
+}
+
+function matchPasswords(string $password, string $hash): bool {
+    return password_verify($password, $hash);
 }
--- src/db.php
+++ src/db.php
@@ -6,7 +6,9 @@ class Db {
     const DB_USER = 'admin';
     const DB_PASSWORD = '123456';
 
-    private PDO $conn;
+    private ?PDO $conn;
+
+    public static ?Db $db = null;
 
     public function __construct() {
         $this->conn = new PDO(
@@ -15,6 +17,40 @@ class Db {
             self::DB_PASSWORD
         );
     }
-}
 
-var_dump(new Db());
+    public function query(string $sql, array $params = []): PDOStatement {
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+
+    /**
+     * Ejecuta una consulta y retorna true si se afectaron filas
+     */
+    public function exec(string $sql, array $params = []): bool
+    {
+        return $this->query($sql, $params)
+            ->rowCount() > 0;
+    }
+
+    /**
+     * Get driver
+     */
+    public function get(): PDO {
+        return $this->conn;
+    }
+
+    /**
+     * Cierra la conexion
+     */
+    public function close() {
+        $this->conn = null;
+    }
+
+    public static function getInstance(): Db {
+        if (self::$db === null) {
+            self::$db = new Db;
+        }
+        return self::$db;
+    }
+}
